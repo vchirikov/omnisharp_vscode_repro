972b0989120baeb5dff7f05413c200ca3f881565
vchirikov <vchirikov@servicetitan.com>
2020-07-10 14:37:01 +0300

    bug repro

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace omnisharp_vscode_repro
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Debugger break");
+        }
+    }
+}
